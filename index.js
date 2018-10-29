ab88a5453087bb78cf831101ff6e1caf230a7cca
Kevin McCartney <hello@kevinmccartney.is>
2018-10-29 18:00:28 -0400

    Updating function to take in delimitors as part of the string

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 class Calculator {
-  static add(numberString, delimiters = [',']) {
+  static add(numberString) {
+    // if the number string is empty, return 0
     if (numberString === '') {
       return 0;
+    } else if (!numberString.startsWith('//')) {
+      return parseInt(numberString);
     }
 
-    let argumentsString = delimiters.reduce((accumulator, currentVal) => {
+    // we need to split the string to find the index of the first number
+    const numberStringSplit = numberString.split('');
+
+    // this is a weird reduce, but we need to return the index of the first number & I want to do it functionally
+    let firstNumberIndex = numberStringSplit.reduce((accumulator, currentVal, currentIndex) => {
+      const parsedValue = parseInt(currentVal);
+      const parsedType = typeof parsedValue;
+      
+      // we have to check that the accumulator is not defined, the parsedValue is not NaN, but that it is a number
+      // (NaN is technically a number - Number.NaN)
+      if (!accumulator && !!parsedValue && parsedType === 'number') {
+        accumulator = currentIndex;
+      }
+
+      return accumulator;
+    }, undefined);
+
+    // grab the string of numbers to parse
+    const numbers = numberString.substring(firstNumberIndex);
+    // get the delimitors (skip the first two //)
+    const delimitors = numberString.slice(2, firstNumberIndex).split('');
+    // pop the last one off (the newline)
+    // TODO: I would love to do this functionally
+    delimitors.pop();
+
+    // this function will split the number string on the delimitor & normalize it with a comma
+    const normalizedString = delimitors.reduce((accumulator, currentVal) => {
       return accumulator.split(currentVal).join(',');
-    }, numberString);
+    }, numbers);
 
-
-    return argumentsString.split(',').reduce((accumulator, currentVal) => accumulator + parseInt(currentVal), 0);
+    // return the final number
+    return normalizedString.split(',').reduce((accumulator, currentVal) => accumulator + parseInt(currentVal), 0);
   }
 }
 
--- index.test.js
+++ index.test.js
@@ -39,23 +39,23 @@ test('Calculator returns the sum of one number correctly', () => {
 });
 
 test('Calculator returns the sum of two numbers correctly', () => {
-  expect(Calculator.add('2,9')).toBe(11);
+  expect(Calculator.add('//,\n2,9')).toBe(11);
 });
 
 test('Calculator returns the sum of an unknown amount of numbers correctly', () => {
-  expect(Calculator.add('1,5,8,14')).toBe(28);
-  expect(Calculator.add('1,5,8,14,3,5')).toBe(36);
+  expect(Calculator.add('//,\n1,5,8,14')).toBe(28);
+  expect(Calculator.add('//,\n1,5,8,14,3,5')).toBe(36);
 });
 
 test('Calculator can handle newlines or commas as delimiters', () => {
-  expect(Calculator.add('3,8,9')).toBe(20);
-  expect(Calculator.add('3\n8\n9', ['\n'])).toBe(20);
+  expect(Calculator.add('//,\n3,8,9')).toBe(20);
+  expect(Calculator.add('//\n\n3\n8\n9')).toBe(20);
 });
 
 test('Calculator can handle mixed newlines or commas as delimiters', () => {
-  expect(Calculator.add('3,1\n12', [',', '\n'])).toBe(16);
+  expect(Calculator.add('//,\n\n3,1\n12')).toBe(16);
 });
 
 test('Calculator can handle mixed newlines or commas as delimiters', () => {
-  expect(Calculator.add('8,5\n12&4+18', [',', '\n', '&', '+'])).toBe(47);
+  expect(Calculator.add('//,\n&+\n8,5\n12&4+18')).toBe(47);
 });
